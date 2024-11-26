b56315d5e1601db0aa61b9b7ed50bd39358d43f6
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-11-26 11:12:37 -0300

    com3

--- prog.py
+++ prog.py
@@ -1,3 +1,8 @@
-print(" -- Programa Iniciado -- ")
+import math as m
 
-print(" -- Programa Concluído -- ")
+print(" -- Programa Iniciado -- \n")
+
+a = m.pi
+print("π = ", a , "\n")
+
+print(" -- Programa Concluído -- \n")
