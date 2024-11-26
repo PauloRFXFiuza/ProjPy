2891a7bb955348bc36a522865b00bcc332bd7a33
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-11-26 11:31:33 -0300

    com5

--- prog.py
+++ prog.py
@@ -8,4 +8,13 @@ print("π = ", a , "\n")
 b = m.sqrt(a)
 print("√π = ", b , "\n")
 
+c = float(input("Insira um numero ao qual sera a base da potenciacao: "))
+print("O numero inserido foi ", c , "\n")
+
+d = float(input("Insira um numero ao qual sera o expoente da potenciacao: "))
+print("O numero inserido foi ", d , "\n")
+
+e = m.pow(c,d)
+print(" ", c , "^" , d ,"=" , e , "\n")
+
 print(" -- Programa Concluído -- \n")
